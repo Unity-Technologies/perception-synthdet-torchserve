d9312309f398268df7d353b848becf9195455e99
Michael Pavkovic <michael.pavkovic@unity3d.com>
2020-07-06 17:48:57 -0400

    Add port number to QR code urls

--- qr.sh
+++ qr.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-IP=$(curl -s icanhazip.com)
 source config.properties
+
+IP=$(curl -s icanhazip.com)
+# Get port number from host with port: https://stackoverflow.com/questions/6174220/parse-url-in-shell-script
+PORT=$(echo $inference_address | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')
 
 SECURE=""
 if [[ $inference_address =~ ^https+ ]]; then
@@ -9,7 +12,7 @@ if [[ $inference_address =~ ^https+ ]]; then
 fi
 
 function print_qr {
-    url=http$SECURE://$IP/predictions/$1
+    url=http$SECURE://$IP:$PORT/predictions/$1
     echo "$1 - $url"
     segno $url
     printf "\n\n\n"
